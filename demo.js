8cf0138b65ea60b696f0267b62b18786211e6775
Jeff Fowler <jeffowler@gmail.com>
2018-09-24 09:32:17 +0200

    move to comments not logging

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const buf = fs.readFileSync('./build/min.wasm');
 
-console.log(buf);
+const buf = fs.readFileSync('./build/min.wasm'); // <Buffer 00 61 73 6d 01 00 00 00>
+const typedBuffer = new Uint8Array(buf); // Uint8Array [ 0, 97, 115, 109, 1, 0, 0, 0 ]
+WebAssembly.validate(typedBuffer); // true
+WebAssembly.instantiate(typedBuffer); // Promise({ instance: Instance {}, module: Module {} })
 
-WebAssembly.instantiate(new Uint8Array(buf)).then(console.log);
+
