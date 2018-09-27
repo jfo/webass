4c532c305ce796261d2907aeb0c893acb7044760
Jeff Fowler <jeffowler@gmail.com>
2018-09-27 11:28:30 +0200

    log differently

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const buf = fs.readFileSync('./build/min.wasm'); // <Buffer 00 61 73 6d 01 00 00 00>
 const typedBuffer = new Uint8Array(buf); // Uint8Array [ 0, 97, 115, 109, 1, 0, 0, 0 ]
-WebAssembly.validate(typedBuffer); // true
-WebAssembly.instantiate(typedBuffer); // Promise({ instance: Instance {}, module: Module {} })
+const valid = WebAssembly.validate(typedBuffer); // true
+const mod = WebAssembly.instantiate(typedBuffer); // Promise({ instance: Instance {}, module: Module {} })
 
-
+mod.then(m => console.log(m));
