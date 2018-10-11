9b052ac2ef76ff411a5f4d48159f649c6645d794
Jeff Fowler <jeffowler@gmail.com>
2018-10-11 10:09:13 +0200

    depr

--- xxd.rb
+++ xxd.rb
@@ -1,8 +1,12 @@
+#!/usr/bin/env ruby
+
 byteArray = File.open('src/min.hex', 'r')
   .read
+  .gsub(/\#.+\n/, '')
   .tr('^10', '')
   .scan(/......../)
   .collect {|s| s.to_i 2}
+
 
 File.open( 'build/min.wasm', 'w' ) do |output|
   byteArray.each do | byte |
